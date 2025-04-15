1ed50840935b4f201e2e3a4a3b10443a251ba57d
Jeff Picklyk <jpicklyk@gmail.com>
2025-04-15 13:27:04 -0400

    ``` Updating dependencies and Kotlin compiler settings
    - Updated AGP to 8.9.1.
    - Updated Android desugar JDK libs to 2.1.5.
    - Updated Android tools to 31.9.1.
    - Updated Kotlin to 2.1.10.
    - Updated KSP to 2.1.10-1.0.29.
    - Set compileSdk to 35.
    - Set minSdk to 31.
    - Added compiler argument `-Xconsistent-data-class-copy-visibility` to address deprecation.
    ```

--- build-logic/convention/src/main/kotlin/internals/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/internals/KotlinAndroid.kt
@@ -27,7 +27,7 @@ import org.gradle.kotlin.dsl.provideDelegate
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
-import org.jetbrains.kotlin.gradle.dsl.KotlinTopLevelExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinBaseExtension
 
 /**
  * Configure base Kotlin with Android options
@@ -36,10 +36,10 @@ internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
     commonExtension.apply {
-        compileSdk = 34
+        compileSdk = 35
 
         defaultConfig {
-            minSdk = 26
+            minSdk = 31
         }
 
         compileOptions {
@@ -75,7 +75,7 @@ internal fun Project.configureKotlinJvm() {
 /**
  * Configure base Kotlin options
  */
-private inline fun <reified T : KotlinTopLevelExtension> Project.configureKotlin() = configure<T> {
+private inline fun <reified T : KotlinBaseExtension> Project.configureKotlin() = configure<T> {
     // Treat all Kotlin warnings as errors (disabled by default)
     // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
     val warningsAsErrors: String? by project
@@ -89,6 +89,20 @@ private inline fun <reified T : KotlinTopLevelExtension> Project.configureKotlin
         freeCompilerArgs.add(
             // Enable experimental coroutines APIs, including Flow
             "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+        )
+        freeCompilerArgs.add(
+            /**
+             * Remove this args after Phase 3.
+             * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-consistent-copy-visibility/#deprecation-timeline
+             *
+             * Deprecation timeline
+             * Phase 3. (Supposedly Kotlin 2.2 or Kotlin 2.3).
+             * The default changes.
+             * Unless ExposedCopyVisibility is used, the generated 'copy' method has the same visibility as the primary constructor.
+             * The binary signature changes. The error on the declaration is no longer reported.
+             * '-Xconsistent-data-class-copy-visibility' compiler flag and ConsistentCopyVisibility annotation are now unnecessary.
+             */
+            "-Xconsistent-data-class-copy-visibility"
         )
     }
 }
